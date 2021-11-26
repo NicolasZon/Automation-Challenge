0464a9ffda0f70f5b42c154a8de4af8e908f2554
Nicolas Sastoque <nicosastoque@gmail.com>
2021-11-26 11:48:10 -0500

    Created gherkin

---
+++ src/test/java/steps/AmazonSteps.java
@@ -0,0 +1,32 @@
+package steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AmazonSteps {
+    @Given("the user navigates to the base page")
+    public void theUserNavigatesToTheBasePage() {
+
+    }
+
+    @When("he searches for {string}")
+    public void heSearchesFor(String arg0) {
+    }
+
+    @And("navigates to the second page")
+    public void navigatesToTheSecondPage() {
+
+    }
+
+    @And("he selects the third item")
+    public void heSelectsTheThirdItem() {
+
+    }
+
+    @Then("the item would be available for purchase")
+    public void theItemWouldBeAvailableForPurchase() {
+
+    }
+}
